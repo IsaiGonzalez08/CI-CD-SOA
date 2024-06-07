36d36b2e312f59e1cd69eeb35b7d9bcea923e2e2
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2024-06-06 22:11:25 -0600

    feat: mensaje

--- test/integrations/test-welcome.js
+++ test/integrations/test-welcome.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../../app.js');
 
 describe('GET /', () => {
-  it('should return Hola mundo!!!!!', (done) => {
+  it('should return Holaa mundo!!!!!', (done) => {
     request(app)
       .get('/')
       .expect(200)
