2c732e191ca797744cd26ba33b5067253b5d1c42
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2024-06-06 21:20:36 -0600

    add dockerfile

--- test/integrations/test-welcome.js
+++ test/integrations/test-welcome.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const app = require('../../app.js');
 
 describe('GET /', () => {
-  it('should return Hola mundo!!!', (done) => {
+  it('should return Hola mundo!!!!', (done) => {
     request(app)
       .get('/')
       .expect(200)
-      .expect('Hola mundo!!!', done);
+      .expect('Hola mundo!!!!', done);
   });
 });
