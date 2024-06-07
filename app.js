c29475eed56142fc79eccebda1645740887daca2
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2024-06-06 21:57:07 -0600

    feat: CHANGE PORT

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hola mundo!!!!!');
 });
 
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
 
 module.exports = app;
